8f8bd7b5f77b18843a82dcc126ff0dcf8a50db9e
FawwazZahranFirzananda <fawwazpsr@gmail.com>
2023-03-15 13:46:44 +0700

    first commit

---
+++ resources/views/experience.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Experience')
+
+@section('content_header')
+<h1>Experience</h1>
+@stop
+
+@section('content')
+<p>Jangan males ngerjain jobsheet ya...</p>
+<h3>Selamat berpusing</h3>
+@stop
+
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('footer')
+<p class="text-center">Fawwaz Zahran Firzananda</p>
+@stop
